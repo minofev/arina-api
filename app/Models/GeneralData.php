b6511c112580dea4bee014164d67406ba5f875b7
Aleksandr <aleksandr@minofev.ru>
2022-02-24 18:49:21 +0300

    fix price_sale (приведение к млн.)

--- app/Models/GeneralData.php
+++ app/Models/GeneralData.php
@@ -22,6 +22,10 @@ class GeneralData extends Model
         static::retrieved(function($model){
             $model->price_actual = round(($model->price_actual/1000000), 2);
             $model->price_actual_to_current_day = round(($model->price_actual/1000000), 2);
+            $model->price_sale = round(($model->price_sale/1000000), 2);
+            $model->price_start = round(($model->price_start/1000000), 2);
+            $model->square_meter_price = round(($model->square_meter_price/1000), 2);
+
             $value = (($model->price_actual-$model->auction)-$model->price_sale);
             if($value > 0)
                 $model->percentage_income = round(($model->price_actual - $model->auction) / $value);
@@ -33,9 +37,6 @@ class GeneralData extends Model
             } else {
                 $model->_izmen = "";
             }
-
-            $model->price_start = round(($model->price_start/1000000), 2);
-            $model->square_meter_price = round(($model->square_meter_price/1000), 2);
 
             $sootn_sign = strpos($model->sootn, '-') === 0 ? '' : '+';
             $model->_soot = $sootn_sign . round($model->sootn) . '%';
